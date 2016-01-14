3a1ed9181a7709a29531394598dda3edd330b8ec
Mike Cannon <michael.j.cannon@gmail.com>
2016-01-14 14:52:28 -0800

    After Heroes tour part 4

---
+++ app/hero.service.ts
@@ -0,0 +1,10 @@
+import {Hero} from './hero';   // Not sure this is necessary
+import {HEROES} from './mock-heroes';
+import {Injectable} from 'angular2/core';
+
+@Injectable()
+export class HeroService {
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+}
