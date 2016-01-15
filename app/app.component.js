9b7df115e3caadfc83dd884899ee9411cc075f42
Mike Cannon <michael.j.cannon@gmail.com>
2016-01-14 22:36:33 -0800

    Move inline css to app/heroes.css

--- app/app.component.js
+++ app/app.component.js
@@ -9,7 +9,7 @@ System.register(['angular2/core', './hero-detail.component', './hero.service', '
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1, hero_detail_component_1, hero_service_1, blocks_service_1;
-    var heroesCss, AppComponent;
+    var AppComponent;
     return {
         setters:[
             function (core_1_1) {
@@ -25,7 +25,6 @@ System.register(['angular2/core', './hero-detail.component', './hero.service', '
                 blocks_service_1 = blocks_service_1_1;
             }],
         execute: function() {
-            heroesCss = "\n.selected {\n  background-color: #CFD8DC !important;\n  color: white;\n}\n\nh1 {\n  text-align: center;\n}\n\n.heroes {\n  margin: 0 0 2em 0;\n  list-style-type: none;\n  padding: 0;\n  width: 10em;\n}\n.heroes li {\n  cursor: pointer;\n  position: relative;\n  left: 0;\n  background-color: #EEE;\n  margin: .5em;\n  padding: .3em 0em;\n  height: 1.6em;\n  border-radius: 4px;\n}\n.heroes li.selected:hover {\n  color: white;\n}\n.heroes li:hover {\n  color: #607D8B;\n  background-color: #EEE;\n  left: .1em;\n}\n.heroes .text {\n  position: relative;\n  top: -3px;\n}\n.heroes .badge {\n  display: inline-block;\n  font-size: small;\n  color: white;\n  padding: 0.8em 0.7em 0em 0.7em;\n  background-color: #607D8B;\n  line-height: 1em;\n  position: relative;\n  left: -1px;\n  top: -4px;\n  height: 1.8em;\n  margin-right: .8em;\n  border-radius: 4px 0px 0px 4px;\n}\n";
             AppComponent = (function () {
                 function AppComponent(_heroService, _blocksService) {
                     this._heroService = _heroService;
@@ -60,7 +59,8 @@ System.register(['angular2/core', './hero-detail.component', './hero.service', '
                     core_1.Component({
                         selector: 'my-app',
                         template: "\n    <h1>{{title}}</h1>\n    <p>{{blockText()}}</p>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"#hero of heroes\" \n          [class.selected]=\"hero === selectedHero\"\n          (click)=\"onSelect(hero)\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    <my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n   ",
-                        styles: [heroesCss],
+                        // styles:[heroesCss],
+                        styleUrls: ['app/heroes.css'],
                         directives: [hero_detail_component_1.HeroDetailComponent],
                         providers: [hero_service_1.HeroService, blocks_service_1.BlocksService]
                     }), 
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,58 +1,3 @@
-var heroesCss = `
-.selected {
-  background-color: #CFD8DC !important;
-  color: white;
-}
-
-h1 {
-  text-align: center;
-}
-
-.heroes {
-  margin: 0 0 2em 0;
-  list-style-type: none;
-  padding: 0;
-  width: 10em;
-}
-.heroes li {
-  cursor: pointer;
-  position: relative;
-  left: 0;
-  background-color: #EEE;
-  margin: .5em;
-  padding: .3em 0em;
-  height: 1.6em;
-  border-radius: 4px;
-}
-.heroes li.selected:hover {
-  color: white;
-}
-.heroes li:hover {
-  color: #607D8B;
-  background-color: #EEE;
-  left: .1em;
-}
-.heroes .text {
-  position: relative;
-  top: -3px;
-}
-.heroes .badge {
-  display: inline-block;
-  font-size: small;
-  color: white;
-  padding: 0.8em 0.7em 0em 0.7em;
-  background-color: #607D8B;
-  line-height: 1em;
-  position: relative;
-  left: -1px;
-  top: -4px;
-  height: 1.8em;
-  margin-right: .8em;
-  border-radius: 4px 0px 0px 4px;
-}
-`;
-
-//-----------------------------------------------------------------------------|
 import {Component, OnInit} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
@@ -75,7 +20,8 @@ import {BlocksService} from './blocks.service';
     </ul>
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
    `,
-  styles:[heroesCss],
+  // styles:[heroesCss],
+  styleUrls:['app/heroes.css'],
   directives: [HeroDetailComponent],
   providers: [HeroService, BlocksService]
 })
