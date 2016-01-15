bb56ff513df598214c8da602d4270a4dff08eba5
Mike Cannon <michael.j.cannon@gmail.com>
2016-01-14 18:10:33 -0800

    Fewer mock-heroes

--- app/mock-heroes.ts
+++ app/mock-heroes.ts
@@ -2,13 +2,13 @@ import {Hero} from './hero';
 
 export var HEROES: Hero[] = [
   { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celery" },
+  // { "id": 12, "name": "Narco" },
+  // { "id": 13, "name": "Bombasto" },
+  // { "id": 14, "name": "Celery" },
   { "id": 15, "name": "Magenta" },
   { "id": 16, "name": "UberMan" },
-  { "id": 17, "name": "Dynama" },
+  // { "id": 17, "name": "Dynama" },
   { "id": 18, "name": "Dr No It All" },
-  { "id": 19, "name": "Magma" },
+  // { "id": 19, "name": "Magma" },
   { "id": 20, "name": "Tournedo" }
 ];
