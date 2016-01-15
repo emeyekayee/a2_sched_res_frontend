7e3b8e4b04bda95a8e7d7d912e9d5fb24b96f732
Mike Cannon <michael.j.cannon@gmail.com>
2016-01-14 21:51:40 -0800

    We have mock-blocks data (less module import)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,70 @@
+var heroesCss = `
+.selected {
+  background-color: #CFD8DC !important;
+  color: white;
+}
+
+h1 {
+  text-align: center;
+}
+
+.heroes {
+  margin: 0 0 2em 0;
+  list-style-type: none;
+  padding: 0;
+  width: 10em;
+}
+.heroes li {
+  cursor: pointer;
+  position: relative;
+  left: 0;
+  background-color: #EEE;
+  margin: .5em;
+  padding: .3em 0em;
+  height: 1.6em;
+  border-radius: 4px;
+}
+.heroes li.selected:hover {
+  color: white;
+}
+.heroes li:hover {
+  color: #607D8B;
+  background-color: #EEE;
+  left: .1em;
+}
+.heroes .text {
+  position: relative;
+  top: -3px;
+}
+.heroes .badge {
+  display: inline-block;
+  font-size: small;
+  color: white;
+  padding: 0.8em 0.7em 0em 0.7em;
+  background-color: #607D8B;
+  line-height: 1em;
+  position: relative;
+  left: -1px;
+  top: -4px;
+  height: 1.8em;
+  margin-right: .8em;
+  border-radius: 4px 0px 0px 4px;
+}
+`;
+
+//-----------------------------------------------------------------------------|
 import {Component, OnInit} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
+import {BlocksService} from './blocks.service';
+
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <p>{{blockText()}}</p>
     <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="#hero of heroes" 
@@ -17,72 +75,41 @@ import {HeroService} from './hero.service';
     </ul>
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
    `,
-  styles:[`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .heroes {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 10em;
-    }
-    .heroes li {
-      cursor: pointer;
-      position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .5em;
-      padding: .3em 0em;
-      height: 1.6em;
-      border-radius: 4px;
-    }
-    .heroes li.selected:hover {
-      color: white;
-    }
-    .heroes li:hover {
-      color: #607D8B;
-      background-color: #EEE;
-      left: .1em;
-    }
-    .heroes .text {
-      position: relative;
-      top: -3px;
-    }
-    .heroes .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0em 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0px 0px 4px;
-    }
-  `],
+  styles:[heroesCss],
   directives: [HeroDetailComponent],
-  providers: [HeroService]
+  providers: [HeroService, BlocksService]
 })
 
 export class AppComponent implements OnInit {
-  public title = 'Tour of Heroes';
+  public title = 'Tour de Liv';
   public heroes: Hero[];
+  public blocks = {};
   public selectedHero: Hero;
 
-  constructor(private _heroService: HeroService) { };
+  constructor(private _heroService: HeroService,
+              private _blocksService: BlocksService) {};
+
+  blockText() {
+    // return "This is text";
+    time_blocks = this.blocks['ZTimeHeaderDay_-8'];
+    return time_blocks ? time_blocks[0].blk.title : "";
+  }
 
   getHeroes() {
     this._heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
-
+  getBlocks() {
+    this._blocksService.getBlocks().then(
+      // blocks => this.blocks = blocks;
+      function (blocks) {
+        this.blocks = blocks;
+      }.bind(this)
+    );
+    // console.debug(this.blocks);
+  }
   ngOnInit() {
     this.getHeroes();
+    this.getBlocks();
   }
-
   onSelect(hero: Hero) { this.selectedHero = hero; }
 }
